7d38b15d58dcd85ec70a95d2c88f81e2dd124d4c
Muhammad Rafliansyah <rafli.ansyahh@mhs.mdp.ac.id>
2022-10-31 15:13:52 +0700

    2

--- app/src/main/java/com/rafli/utsmuhammadrafliansyah/MainActivity.java
+++ app/src/main/java/com/rafli/utsmuhammadrafliansyah/MainActivity.java
@@ -2,13 +2,28 @@ package com.rafli.utsmuhammadrafliansyah;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button btndaftar;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btndaftar = findViewById(R.id.btn_daftar);
+        btndaftar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent daftar = new Intent(MainActivity.this, SecondActivity.class);
+                startActivity(daftar);
+            }
+        });
     }
 }
